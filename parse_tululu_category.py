170a3567e63da112184e2c6aaf79d9fbcfa399eb
Mishecha <laptevm472@gmail.com>
2023-09-19 18:02:21 +0300

    Поправил множественное число в названиях

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         os.makedirs('dir_books', exist_ok=True)
     if not args.skip_img:
         os.makedirs('dir_images', exist_ok=True)
-    book = []
+    books = []
     for number in range(args.start_page, args.end_page):
         book_url = f'https://tululu.org/l55/{number}/'
 
@@ -70,7 +70,7 @@ if __name__ == '__main__':
                 heading = book_content['book_name']
                 book_photo = book_content['image_link']
                 photo_link = urljoin(f'http://tululu.org/b{number_id}/', book_photo)
-                book.append(book_content)
+                books.append(book_content)
                 if not args.skip_txt:
                     download_txt(number_id, heading)
                 if not args.skip_img:
